2a9494220e7aa07194da22abf0f915f3a0f4fec3
Adeel Ishfaq <magento@magentoxpert.com>
2016-03-09 01:42:29 +0500

    Order Export Time Filter
    Set time filter dynamically from backend.

--- app/code/community/Sharplogicians/Orderexporter/Model/Observer.php
+++ app/code/community/Sharplogicians/Orderexporter/Model/Observer.php
@@ -5,7 +5,10 @@ class Sharplogicians_Orderexporter_Model_Observer {
 	{
 	 	$time = time();
 		$to = date('Y-m-d H:i:s', $time);
-		$lastTime = $time - 86400; // 60*60*24
+		$minusTime = Mage::getStoreConfig('sharplogicians/orderexportcron/timevalue',Mage::app()->getStore());
+		//Mage::log($minusTime);
+		//$lastTime = $time - 86400; // 60*60*24
+		$lastTime = $time - $minusTime;
 		$from = date('Y-m-d H:i:s', $lastTime);
 	  	$orders = Mage::getModel('sales/order')->getCollection()
 		->addAttributeToSelect('entity_id')
